c08440fbdcbda81e15602f431ca5646d0f794545
Timofte Andrei Teodor <timofteandreiteodor@EN1210756.local>
2021-08-01 16:08:31 +0300

    improve ajax concurent calls to be 365 at a time

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-
 app.use('/data', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
-})
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 const getFormattedDate = (date) => {
   const d = new Date(date);
@@ -24,9 +23,11 @@ const getFormattedDate = (date) => {
 };
 
 const fetchCursBnrRo = async (DATE) => {
-  const CURS_BNR_RO_DAY_URL = 'https://www.cursbnr.ro/arhiva-curs-bnr-$DATE'
+  const CURS_BNR_RO_DAY_URL = 'https://www.cursbnr.ro/arhiva-curs-bnr-$DATE';
   try {
-    const response = await axios.get(CURS_BNR_RO_DAY_URL.replace('$DATE', DATE))
+    const response = await axios.get(
+      CURS_BNR_RO_DAY_URL.replace('$DATE', DATE)
+    );
     const $ = cheerio.load(response.data);
 
     const EURO_ROW = $('table tbody tr td:contains("EUR")');
@@ -34,12 +35,13 @@ const fetchCursBnrRo = async (DATE) => {
     return {
       date: DATE,
       currency: EURO_ROW.text(),
-      value: parseFloat(EURO_ROW.next().next().text(), 10)
-    }
+      value: parseFloat(EURO_ROW.next().next().text(), 10),
+    };
+  } catch (err) {
+    console.error(
+      `ERROR: An error occurred while trying to fetch: ${(err, DATE)}`
+    );
   }
-  catch (err) {
-    console.error(`ERROR: An error occurred while trying to fetch: ${err, DATE}`);
-  };
 };
 
 const scrapeCursBnrRo = async () => {
@@ -48,39 +50,60 @@ const scrapeCursBnrRo = async () => {
     const jsonData = JSON.parse(fs.readFileSync(JSON_FILE_NAME));
 
     if (jsonData && Object.keys(jsonData).length) return;
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err);
   }
 
   const URL_DATE = new Date('2005-01-03');
   const CURRENT_DATE = new Date();
-  const results = [];
+  let results = [];
   const avgResults = {
     labels: [],
     data: [],
-    currency: 'EUR'
+    currency: 'EUR',
   };
 
+  let promisesArray = [];
+  let promisesCounter = 0;
+
   while (URL_DATE < CURRENT_DATE) {
-    const result = await fetchCursBnrRo(getFormattedDate(URL_DATE));
-    results.push(result);
-    URL_DATE.setDate(URL_DATE.getDate() + 1);
+    if (promisesCounter <= 365) {
+      promisesArray.push(fetchCursBnrRo(getFormattedDate(URL_DATE)));
+      URL_DATE.setDate(URL_DATE.getDate() + 1);
+      promisesCounter += 1;
+    } else {
+      const promisesResults = await Promise.all(promisesArray);
+      results = results.concat(promisesResults);
+
+      promisesArray = [];
+      promisesCounter = 0;
+    }
+  }
+
+  if (promisesArray.length) {
+    const promisesResults = await Promise.all(promisesArray);
+    results = results.concat(promisesResults);
   }
 
   let sum = 0;
   let noOfDays = 0;
   let month = '' + (new Date(results[0].date).getMonth() + 1);
+  let year = '' + new Date(results[0].date).getFullYear();
+
   for (let index = 1; index < results.length; index++) {
     const currentMonth = '' + (new Date(results[index].date).getMonth() + 1);
+    const currentYear = '' + new Date(results[index].date).getFullYear();
+
     if (month !== currentMonth) {
-      const year = new Date(results[index].date).getFullYear();
-      if (month.length < 2) month = '0' + month;
+      month = month.length === 1 ? '0' + month : month;
+
       avgResults.labels.push([year, month].join('-'));
       avgResults.data.push(sum / noOfDays);
-      sum = 0;
-      noOfDays = 0;
-      month = currentMonth
+
+      sum = results[index].value;
+      noOfDays = 1;
+      month = currentMonth;
+      year = currentYear;
     } else {
       sum += results[index].value;
       noOfDays += 1;
@@ -92,8 +115,7 @@ const scrapeCursBnrRo = async () => {
 };
 scrapeCursBnrRo();
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
